0e8d0c93056a32922cf6418460cc28872691803b
KimChol <kimchol2ece@outlook.com>
2020-11-06 12:48:51 +0800

    setcookie设置过期时间

--- stu/views.py
+++ stu/views.py
@@ -7,5 +7,6 @@ def setcookie(request):
     # 创建响应对象
     response = HttpResponse()
     # 将数据存储在cookie中
-    response.set_cookie('uname', 'zhangsan')
+    # 默认有效时间 保存浏览器缓存中 关闭浏览器数据丢失,max_age方法可更改有效时间，单位为秒。24 * 60 * 60代表24小时乘60分乘60秒
+    response.set_cookie('uname', 'zhangsan', max_age=24 * 60 * 60)
     return response
